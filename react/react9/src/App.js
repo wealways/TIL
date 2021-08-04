71c6cc8270d620610cff0943ab458bae03821995
seungju <likeletter5658@gmail.com>
2021-08-04 15:49:58 +0900

    Commit befor yarn eject

---
+++ react/react9/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import SassComponent from './components/SassComponent'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />        
+      </div>
+    )
+  }
+}
