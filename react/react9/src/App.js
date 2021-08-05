09062bb6cde60ed41f6d6349c9a3cb41c28bb0bc
seungju <likeletter5658@gmail.com>
2021-08-05 09:05:17 +0900

    Add react9
    styled-component에 대해

--- react/react9/src/App.js
+++ react/react9/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import SassComponent from './components/SassComponent'
+// import SassComponent from './components/SassComponent'
+import StyledComponent from './components/StyledComponent'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />        
+        {/* <SassComponent />         */}
+        <StyledComponent />
       </div>
     )
   }
